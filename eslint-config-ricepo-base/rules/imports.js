08952599602864afb1fe357eafcf56eb87005382
Denis Luchkin-Zhou <denis@ricepo.com>
2016-07-29 17:31:49 -0700

    import/prefer-default-export: 0

--- eslint-config-ricepo-base/rules/imports.js
+++ eslint-config-ricepo-base/rules/imports.js
@@ -150,7 +150,7 @@ module.exports = {
      * Require modules with a single export to use a default export
      * https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/prefer-default-export.md
      */
-    'import/prefer-default-export': 2,
+    'import/prefer-default-export': 0,
 
 
     /**
