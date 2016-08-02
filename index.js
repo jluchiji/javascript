92e7b7c135c9405a3fe23f90f4174cfdcb0d9a7c
Denis Luchkin-Zhou <denis@ricepo.com>
2016-08-02 01:10:41 -0700

    Added ava rules to the default config

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-module.exports = {
-  extends: [
-    './base',
-    './react',
-    './lodash'
-  ].map(require.resolve),
-  rules: { }
-};
+/**
+ * index.js
+ *
+ * @author  Denis Luchkin-Zhou <denis@ricepo.com>
+ * @license 2015-16 (C) Ricepo LLC. All Rights Reserved.
+ */
+
+exports.extends = [
+
+  './base',
+  './react',
+  './lodash',
+  './ava'
+
+].map(require.resolve);
